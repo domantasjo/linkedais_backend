5875bf1bf443cc8a7ac5626409ae3e2e61a56606
Domantas Jocas <domantas0216@gmail.com>
2026-02-09 13:42:27 +0200

    first commit

---
+++ src/main/java/com/linkedais/backend/LinkedaisBackendApplication.java
@@ -0,0 +1,13 @@
+package com.linkedais.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinkedaisBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LinkedaisBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/linkedais/backend/LinkedaisBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.linkedais.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LinkedaisBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
